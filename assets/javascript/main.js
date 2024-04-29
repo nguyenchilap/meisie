de758bde08570b32425a7174d64fd2b1f91d5ac6
nguyenchilap <lapnguyen18620@gmail.com>
2024-04-29 12:14:22 +0700

    'initialize'

---
+++ assets/javascript/main.js
@@ -0,0 +1,182 @@
+let FILTER_SELECTED = {
+    photos: 'cogiao',
+    posts: 'cap2'
+}
+
+let MUSIC_SELECTED_INDEX = 0;
+
+
+
+function init() {
+    if (renderPhotos(FILTER_SELECTED.photos)) {
+        $(`.filter__item-link[filterValue="${FILTER_SELECTED.photos}"]`).addClass('active');
+    };
+    responsive();
+    handleScroll();
+    renderTopicMusic();
+    renderListMusic();
+    // handleMusicItemBtn();
+}
+
+function renderPhotos(filterValue) {
+    if (!LIST_PHOTO[filterValue]) {
+        return false; 
+    }
+    if (LIST_PHOTO[filterValue].photos.length <= 0) {
+        $('#list-photo').append(`
+            <h2>CHƯA CÓ ẢNH ĐỂ HIỂN THỊ</h2>
+        `);
+        return false;;
+    }
+    $('#list-photo').html('');
+    $('.content__title').html(LIST_PHOTO[filterValue].title)
+
+    LIST_PHOTO[filterValue].photos.forEach(function(photo, index) {
+        let itemImg = '';
+        if (photo.img) {
+            itemImg = `<div class="item__img" style="background-image: url('./assets/img/photos/${filterValue}/${filterValue}_${index}.jpg')"></div>`;
+        } else if (photo.imgs) {
+            itemImg = `
+                <div class="item__imgs" albumIndex="${index}" currentIndex="${0}">
+                    <button class="btn btn-item-img btn-item-img-left">
+                        <i class="fa-solid fa-caret-left"></i>
+                    </button>
+            `
+            photo.imgs.forEach((img, i) => {
+                if (i == 0) itemImg += `<div class="item__img active item__img-${i}" style="background-image: url(${img})"></div>`;
+                else itemImg += `<div class="item__img item__img-${i}" style="background-image: url(${img})"></div>`
+            });
+
+            itemImg += `
+                    <button class="btn btn-item-img btn-item-img-right">
+                        <i class="fa-solid fa-caret-right"></i>
+                    </button>
+                </div>
+            `
+        }
+        $('#list-photo').append(`
+            <div class="col-3">
+                <div class="container__item">
+                    ${itemImg}
+                    <a href="${photo.url}" target="_blank" class="item__detail">
+                        <div class="item__review">${photo.title}</div>
+                        <div class="item__date">${photo.date}</div>
+                        <div class="item__footer">
+                            <div class="item__react item__react--liked">
+                                <i class="item__react-like-icon far fa-heart"></i>
+                                <i class="item__react-liked-icon fas fa-heart"></i>
+                            </div>
+                            <div class="item__location">${photo.location}</div>
+                        </div>
+
+                        <div class="item__label-favor">
+                            <i class="item__label-favor-icon fas fa-crown"></i>
+                            <span class="item__label-favor-text">Favorite</span>
+                        </div>
+                    </a>
+                </div>
+            </div>  
+        `);
+    });
+    handleItemImgBtn();
+    return true;
+}
+
+function renderTopicMusic() {
+    $('audio.topic__sound-audio').attr('src', MUSICS[MUSIC_SELECTED_INDEX].url);
+    $('.topic__sound-name').html(MUSICS[MUSIC_SELECTED_INDEX].name);
+    $('.topic__sound-img').css('background-image',`url('${MUSICS[MUSIC_SELECTED_INDEX].img}')`);
+}
+
+function renderListMusic() {
+    $('ul.nav__music-list').html('');
+    MUSICS.forEach((music, index) => {
+        $('ul.nav__music-list').append(`
+            <li class="nav__music-item" onclick="onMusicItemBtn(event, ${index})">
+                <div class="nav__music-item-link">
+                    <img src="${music.img}" alt="page-img" class="nav__music-item-img">
+                    <div class="nav__music-item-name">${music.name}</div>
+                    <div class="nav__music-item-author">${music.author}</div>
+                </div>
+            </li>
+        `);
+    })
+}
+
+function onMusicItemBtn(event, index) {
+    $('.nav__music-item').removeClass('active');
+    event.target.classList.add('active');
+    MUSIC_SELECTED_INDEX = index;
+    renderTopicMusic();
+}
+
+function handleScroll() {
+    $(window).scroll(() => {
+        if($(window).scrollTop() + $(window).height() >= ($('#photos').height() - 120)/2) {
+            $('.btn-scroll-top-content').addClass('active');
+        } else {
+            $('.btn-scroll-top-content').removeClass('active');
+        }
+    });
+
+    $('.btn-scroll-top-content').click(() => {
+        $('html, body').animate({
+            scrollTop: $("#photos").offset().top
+        }, 100);
+    })
+}
+
+function handleItemImgBtn() {
+    $('.btn-item-img-left').click((e) => {
+        const parent = $(e.target).parents('.item__imgs');
+        const albumIndex = parent.attr('albumIndex');
+        let currentIndex = Number.parseInt(parent.attr('currentIndex'));
+
+        if (currentIndex == 0) {
+            return;
+        }
+
+        currentIndex -= 1;
+
+        parent.attr('currentIndex', currentIndex);
+        $(`.item__imgs[albumIndex="${albumIndex}"] .item__img`).removeClass('active');
+        $(`.item__imgs[albumIndex="${albumIndex}"] .item__img-${currentIndex}`).addClass('active');
+    })
+
+    $('.btn-item-img-right').click((e) => {
+        const parent = $(e.target).parents('.item__imgs');
+        const albumIndex = parent.attr('albumIndex');
+        let currentIndex = Number.parseInt(parent.attr('currentIndex'));
+
+        if (currentIndex == LIST_PHOTO[FILTER_SELECTED.photos].photos[albumIndex].imgs.length - 1) {
+            return;
+        }
+
+        currentIndex += 1;
+
+        parent.attr('currentIndex', currentIndex);
+        $(`.item__imgs[albumIndex="${albumIndex}"] .item__img`).removeClass('active');
+        $(`.item__imgs[albumIndex="${albumIndex}"] .item__img-${currentIndex}`).addClass('active');
+    })
+}
+
+
+
+$(document).ready(function() {
+
+    init();
+
+    $('.filter__item-link').click(function() {
+        FILTER_SELECTED.photos = $(this).attr('filterValue');
+        if (renderPhotos(FILTER_SELECTED.photos)) {
+            $('.filter__item-link').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+    $(window).resize(function() {
+        responsive();
+    }) 
+    
+})
+
---
+++ assets/javascript/topic.js
@@ -0,0 +1,133 @@
+
+//TOAST
+function toast({
+    title = '',
+    message = '',
+    type = 'info',
+    duration = 1000
+})
+{
+    const main = document.getElementById('toast');
+    if (main){
+        const toast = document.createElement('div');
+        
+        // auto remove toast
+        const autoRemoveId = setTimeout(function(){
+            main.removeChild(toast);
+        }, duration + 1000);
+
+        // remove toast after click
+        toast.onclick = function(e) {
+            if (e.target.closest('.custom-toast__close')){
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            };
+        }
+        
+        const icons = {
+            love: 'fab fa-gratipay',
+            like: 'fas fa-thumbs-up',
+            angry: 'fas fa-angry',
+        };
+
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+
+        toast.classList.add('custom-toast', `custom-toast--${type}`);
+        toast.style.animation = `slideInLeft ease 0.3s, fadeOut linear 1s ${delay}s forwards`;
+        
+        toast.innerHTML = `
+            <div class="custom-toast__icon">
+                <i class="${icon}"></i>
+            </div>
+            <div class="custom-toast__body">
+                <h3 class="custom-toast__title">${title}</h3>
+                <div class="custom-toast__msg">${message}</div>
+            </div>
+            <div class="custom-toast__close">
+                <i class="fas fa-times"></i>
+            </div>
+        `;
+        main.appendChild(toast);
+    }
+}
+
+function showLoveToast(){
+    toast({
+        title: 'Love',
+        message: 'Yêu Mây',
+        type: 'love',
+        duration: 2000,
+    })
+}
+function showLikeToast(){
+    toast({
+        title: 'Like',
+        message: 'Thích Mây',
+        type: 'like',
+        duration: 2000,
+    })
+}
+
+function showAngryToast(){
+    toast({
+        title: 'Angry',
+        message: 'Dận Mây',
+        type: 'angry',
+        duration: 2000,
+    })
+}
+
+//TOPIC - MUSIC
+{
+    const audio =  document.querySelector('.topic__sound-audio');
+    const img = document.querySelector('.topic__sound-img'); 
+    const control = document.querySelector('.topic__sound-control');
+    const playBtn = document.querySelector('i.topic__sound-icon--play');
+    const pauseBtn = document.querySelector('i.topic__sound-icon--pause');
+
+
+    const app = {
+        isPausing: true,
+
+        handleEvent: function(){
+            const _this = this;
+
+            img.onclick = function(){
+                playBtn.style.animation = "fadeOut ease 1s, growthOver ease 1s";
+                pauseBtn.style.animation = "fadeOut ease 1s, growthOver ease 1s";
+                _this.isPausing = !_this.isPausing;
+                _this.checkPlaying();
+
+                control.classList.toggle('pausing', _this.isPausing);
+            }
+        },
+
+        checkPlaying: function(){
+            if(this.isPausing === false) {
+                audio.play();
+            }
+            else audio.pause();
+        },
+
+        start: function(){
+            audio.loop = true;
+            //Handle Event
+            this.handleEvent();
+            this.autoAvatarSlider();
+        },
+
+        autoAvatarSlider: function() {
+            let count = 1;
+            $(`img.topic__avatar-img-${count}`).addClass('active');
+            setInterval(function(){
+                if (count == 7) count = 1;
+                count++;
+                $('img.topic__avatar-img').removeClass('active');
+                $(`img.topic__avatar-img-${count}`).addClass('active');
+            }, 1500);
+        }
+    }
+
+    app.start();
+}
